7b30d52691926c90752f343019a04b6c12b4673a
shamita1512 <64206512+shamita1512@users.noreply.github.com>
2020-04-29 17:46:48 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,27 @@
+var movingRect, fixedRect;
+function setup() {
+  createCanvas(800,400);
+  movingRect = createSprite(400, 200, 50, 50);
+  movingRect.shapeColor="green";
+  fixedRect = createSprite(200,200,60,70);
+  fixedRect.shapeColor = "green";
+}
+
+function draw() {
+  background("blue"); 
+  movingRect.x = mouseX;
+  movingRect.y = mouseY;
+  if(movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2 
+    && fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2
+    && movingRect.y - fixedRect.y<movingRect.height/2+fixedRect.height/2
+    && fixedRect.y - movingRect.y<movingRect.height/2+fixedRect.height/2){
+    movingRect.shapeColor = "white";
+    fixedRect.shapeColor = "white";
+    
+  }
+  else{
+    movingRect.shapeColor = "green";
+    fixedRect.shapeColor = "green";
+  }
+  drawSprites();
+}
